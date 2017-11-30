0c3143383f0f7fe6d16e05146c9e6ba28d574c2d
Shiv singh <shiv007singh@gmail.com>
2017-11-30 17:35:15 +0530

    resolved project link issue

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.get('/about', (req, res) => {
 })
 
 
-app.get('/proejcts', (req, res) => {
-    res.render('proejct.hbs', {
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
         pageTitle: 'Projects'
     });
 })
